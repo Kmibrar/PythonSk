118f79b0ebdd56a6f3440c5f1888bae7784ae5aa
ibrar shah <ibrarshsh365@gmail.com>
2023-07-23 13:05:49 +0500

    Basic of Tuple Different types of Creating Tuple

--- Sets/Basic.py
+++ Sets/Basic.py
@@ -14,3 +14,26 @@
 # and can be accessed through indexing and slicing.
 
 # Contains Duplicates: Tuples can contain duplicates, which means they can have items with the same value
+
+# Basic
+# create a tuple using ()
+# number tuple
+number_tuple = (10, 20, 25.75)
+print(number_tuple)
+# Output (10, 20, 25.75)
+
+# string tuple
+string_tuple = ('Jessa', 'Emma', 'Kelly')
+print(string_tuple)
+# Output ('Jessa', 'Emma', 'Kelly')
+
+# mixed type tuple
+sample_tuple = ('Jessa', 30, 45.75, [25, 78])
+print(sample_tuple)
+# Output ('Jessa', 30, 45.75, [25, 78])
+
+# create a tuple using tuple() constructor
+sample_tuple2 = tuple(('Jessa', 30, 45.75, [23, 78]))
+print(sample_tuple2)
+# Output ('Jessa', 30, 45.75, [23, 78])
+
